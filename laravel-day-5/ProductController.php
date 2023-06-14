44e2834e1290b55f286ee026d78a42291ac59fca
fahimtalukdarakash <77202043+fahimtalukdarakash@users.noreply.github.com>
2023-06-14 13:12:32 +0600

    Add files via upload

---
+++ laravel-day-5/ProductController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use DB;
+
+class ProductController extends Controller
+{
+    private $products,$product;
+    public function index()
+    {
+        return view('product.add');
+    }
+    public function create(Request $request)
+    {
+        Product::newProduct($request);
+        return back()->with('message','Product info save successfully');
+    }
+    public function manage()
+    {
+        $this->products = Product::all();
+        return view('product.manage',['products'=>$this->products]);
+    }
+    public function updatePage($id)
+    {
+        $this->product = Product::find($id);
+        return view('product.update',['product' => $this->product]);
+    }
+    public function update(Request $request, $id)
+    {
+        Product::updateProduct($request,$id);
+        return redirect('/manage-product')->with('message', 'Product info update successfully');
+    }
+    public function delete($id)
+    {
+        Product::deleteProduct($id);
+        return back()->with('message', 'Product info delete successfully');
+    }
+}
